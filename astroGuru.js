7b7cc057b3dabd204b9e55c2a884a465371f20fd
johnwickcodes <115461555+johnwickcodes@users.noreply.github.com>
2022-11-17 17:24:34 +0530

    Add files via upload
    Problem Statement - 1
    Write a function astroguru that:
    Takes four arguments: number of children, spouses name, location and job
    Output the following string: “You will be a ‘job’ in ‘location’ and married to ‘partner’ with ‘numkids’.”

---
+++ astroGuru.js
@@ -0,0 +1,11 @@
+/* Problem Statement - 1
+Write a function astroguru that:
+Takes four arguments: number of children, spouses name, location and job
+Output the following string: “You will be a ‘job’ in ‘location’ and married to ‘partner’ with ‘numkids’.”
+
+*/
+console.log("Welcome to AstroGuru");
+function astroguru(job, numkid, partner, location){
+    console.log("You will be a "+job+" living in "+location+" married to "+partner+", with "+numkid+" little children")
+}
+astroguru("Software Entreprenure",369 ,"Sudha","London");
