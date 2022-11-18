05e1d6866c3099f337dc85f535a517cffd4c53a9
johnwickcodes <115461555+johnwickcodes@users.noreply.github.com>
2022-11-18 18:16:54 +0530

    Assignment2

--- astroGuru.js
+++ astroGuru.js
@@ -9,3 +9,31 @@ function astroguru(job, numkid, partner, location){
     console.log("You will be a "+job+" living in "+location+" married to "+partner+", with "+numkid+" little children")
 }
 astroguru("Software Entreprenure",369 ,"Sudha","London");
+
+/*
+
+Problem Statement - 2
+Write a function that compares two numbers and returns the larger of the two.
+Bonus: Output appropriate message if numbers cant be compared or are equal
+
+*/
+
+function compare(num1,num2){
+    if(num1===num2){
+        console.log(num1," and ",num2, " are equal")
+    }
+    else if(num1>num2){
+       
+        console.log(num1, " is greater than ", num2)
+    }
+    else if(num2>num1){
+        console.log(num2, " is greater than ", num1)
+    }
+    else {console.log("The given input is incompareable")}
+}
+
+
+compare(50,78);
+compare(79,677);
+compare(78,78);
+compare("Rishabh", 67);
